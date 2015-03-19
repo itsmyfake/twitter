03d490272fb6306b14108bab043968fbe61d5694
Alexey <lipkinalexey@gmail.com>
2015-03-19 09:14:21 +0000

    Add Folowing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   
   root "static_pages#home"
-  resources :users
+  resources :users 
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
